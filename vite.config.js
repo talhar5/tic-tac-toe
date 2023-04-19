73f0835c951e5185650685ad46aefcbb2ef8c523
talha <trazzaq9@gmail.com>
2023-04-20 01:37:28 +0500

    static path fixed

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 const path = require('path')
 
 export default {
-  root: path.resolve(__dirname, 'src'),
+  root: path.resolve(__dirname, './src/'),
   build: {
     outDir: '../dist'
   },
-  base: "/tic-tac-toe/",
+  base: "./",
   server: {
     port: 8080,
     hot: true
